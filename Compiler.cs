cb125d08462caf231ae235335a49b91049297612
Andreas.Kurka <andreas.kurka@gmail.com>
2024-12-29 19:02:58 +0100

    Added a sketch for a compiler class

---
+++ Compiler.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System;
+
+public class MetaData
+{
+    public string Version { get; set; } // Meta file versioning
+    public SPBHeaderMeta Header { get; set; }
+    public List<SPBTagMeta> Tags { get; set; }
+    public List<SPBSetMeta> Sets { get; set; }
+}
+
+public class SPBHeaderMeta
+{
+    public ushort FileType { get; set; }
+    public int[] HeaderValues { get; set; } // Array of 12 header integers
+}
+
+public class SPBTagMeta
+{
+    public Guid TagGuid { get; set; }
+    public int UnknownFlag { get; set; }
+}
+
+public class SPBSetMeta
+{
+    public string SetName { get; set; }
+    public int SetSize { get; set; }
+    public long SetOffset { get; set; } // Optional for validation
+}
+
+public class MetaFileHandler
+{
+    public static MetaData ReadMetaFile(string metaFilePath)
+    {
+        // Deserialize the meta file into a MetaData object
+        // (XML or binary reading logic goes here)
+        return null;
+    }
+
+    public static void WriteMetaFile(string metaFilePath, MetaData metaData)
+    {
+        // Serialize the MetaData object into a meta file
+        // (XML or binary writing logic goes here)
+    }
+}
+
+public class Compiler
+{
+    public static void Compile(string xmlFilePath, string metaFilePath, string outputFilePath)
+    {
+        MetaData metaData = MetaFileHandler.ReadMetaFile(metaFilePath);
+
+        using (BinaryWriter writer = new BinaryWriter(File.Create(outputFilePath)))
+        {
+            // Write header
+            writer.Write(metaData.Header.FileType);
+            foreach (var value in metaData.Header.HeaderValues)
+            {
+                writer.Write(value);
+            }
+
+            // Write tags
+            foreach (var tag in metaData.Tags)
+            {
+                writer.Write(tag.TagGuid.ToByteArray());
+                writer.Write(tag.UnknownFlag);
+            }
+
+            // Process and write sets using XML data + meta information
+            // Logic to combine XML and meta data
+        }
+    }
+}
